4d9ce08a67e047386d4b02cf7d4e20aea20c62b0
rhayk <rhayk@10.7.26.27>
2019-09-20 21:36:49 -0300

    O que foi aprendido até no momento: *Objetivo de negócio. *Características. *Exemplos na prática. *Específicações executáveis.

---
+++ CursoCucumber01/src/test/java/AprenderCucumber.java
@@ -0,0 +1,23 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+//Ctrl+shift+O -> Importa as apis
+
+public class AprenderCucumber {
+	
+	@Given("^Dado que criei o arquivo corretamente$")
+	public void dado_que_criei_o_arquivo_corretamente() throws Throwable {
+	   
+	}
+
+	@When("^Quando execut?-lo$")
+	public void quando_execut_lo() throws Throwable {
+	
+	}
+
+	@Then("^Ent?o a especifica??o deve finalizar com sucesso$")
+	public void ent_o_a_especifica_o_deve_finalizar_com_sucesso() throws Throwable {
+	 
+	}
+}
---
+++ CursoCucumber01/src/test/java/aprender_cucumber.feature
@@ -0,0 +1,10 @@
+# language: pt
+Funcionalidade: Aprender Cucumber
+  	Como um aluno
+	  Eu quero aprender a utilizar cucumber	
+		Para que eu possa automatizar citérios de aceitação
+
+Cenário: 
+    Dado que criei o arquivo corretamente
+	  Quando executá-lo
+  	Então a especificação deve finalizar com sucesso
